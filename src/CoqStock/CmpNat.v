d88e1287205c1c8b971b796332469336f75fa84e
Walter Schulze <awalterschulze@gmail.com>
2022-01-08 17:00:39 +0000

    rewrite Nat.compare to compare

--- src/CoqStock/CmpNat.v
+++ src/CoqStock/CmpNat.v
@@ -52,8 +52,10 @@ Lemma nat_proof_compare_eq_reflex
   : forall (x: nat)
   , nat_compare x x = Eq.
 Proof.
-(* TODO *)
-Admitted.
+induction x.
+- reflexivity.
+- cbn. assumption.
+Qed.
 
 Lemma nat_proof_compare_eq_trans
   : forall (x y z: nat)
@@ -61,8 +63,12 @@ Lemma nat_proof_compare_eq_trans
            (q: nat_compare y z = Eq)
   , nat_compare x z = Eq.
 Proof.
-(* TODO *)
-Admitted.
+unfold nat_compare.
+intros.
+rewrite PeanoNat.Nat.compare_eq_iff in *.
+subst.
+reflexivity.
+Qed.
 
 Lemma nat_proof_compare_lt_trans
   : forall (x y z: nat)
@@ -70,8 +76,11 @@ Lemma nat_proof_compare_lt_trans
            (q: nat_compare y z = Lt)
   , nat_compare x z = Lt.
 Proof.
-(* TODO *)
-Admitted.
+unfold nat_compare.
+intros.
+rewrite PeanoNat.Nat.compare_lt_iff in *.
+exact (PeanoNat.Nat.lt_trans x y z  p q).
+Qed.
 
 Lemma nat_proof_compare_gt_trans
   : forall (x y z: nat)
@@ -79,9 +88,13 @@ Lemma nat_proof_compare_gt_trans
            (q: nat_compare y z = Gt)
   , nat_compare x z = Gt.
 Proof.
-(* TODO *)
-Admitted.
+unfold nat_compare.
+intros.
+rewrite PeanoNat.Nat.compare_gt_iff in *.
+exact (PeanoNat.Nat.lt_trans z y x q p).
+Qed.
 
+#[export]
 Instance CmpNat : Cmp nat :=
   { compare := nat_compare
   ; proof_compare_eq_implies_equal := nat_proof_compare_eq_implies_equal
@@ -90,3 +103,32 @@ Instance CmpNat : Cmp nat :=
   ; proof_compare_lt_trans := nat_proof_compare_lt_trans
   ; proof_compare_gt_trans := nat_proof_compare_gt_trans
   }.
+
+Theorem nat_compare_is_compare:
+  forall
+    {x y: nat}
+    {c: comparison},
+  Nat.compare x y = c <-> compare x y = c.
+Proof.
+split; intros; assumption.
+Qed.
+
+Ltac rewrite_nat_compare_to_compare :=
+  repeat match goal with
+  | [ |- context [Nat.compare _ _]] =>
+    rewrite nat_compare_is_compare
+  | [ H: context [Nat.compare _ _]|- _ ] =>
+    rewrite nat_compare_is_compare in H
+  end.
+
+Example example_nat_compare_to_compare:
+  forall
+    {x y: nat}
+    (H: Nat.compare x y = Gt),
+  Nat.compare y x = Lt.
+Proof.
+intros.
+rewrite_nat_compare_to_compare.
+apply compare_gt_lt_symm.
+assumption.
+Qed.
