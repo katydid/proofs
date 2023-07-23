64f9908a96c7c40beeee84702ac8bda5c31ea496
Paul Cadman <git@paulcadman.dev>
2023-07-23 20:44:55 +0100

    Add dependency on mathlib4
    The mathlib4 documentation recommends that you do not add std4 and
    quote4 as separate dependencies, instead obtain these transitively
    through the mathlib4 dependency. This is so that `lake exe cache` works
    reliably.
    
    https://github.com/leanprover-community/mathlib4/wiki/Using-mathlib4-as-a-dependency/712f7e92c5a68ab4b8601192f86e3c8ae0dc7f33#more-on-lake-exe-cache

--- lakefile.lean
+++ lakefile.lean
@@ -6,5 +6,6 @@ package katydid
 @[default_target]
 lean_lib Katydid
 
-require std from git "https://github.com/leanprover/std4" @ "main"
-require Qq from git "https://github.com/gebner/quote4" @ "master"
+-- dependencies std4, quote4 are obtained transitively through mathlib4
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4"
