49aa2423743e59ae468c3922575af787522d9af9
Paul Cadman <git@paulcadman.dev>
2024-01-06 23:28:52 +0000

    Add Tiso

--- Katydid/Std/Tipe.lean
+++ Katydid/Std/Tipe.lean
@@ -9,13 +9,15 @@ import Mathlib.Init.Algebra.Classes
 The equality relation. We use this instead of Lean's `Eq` because
 we need it to be defined on Type instead of Prop.
 -/
-inductive TEq {α : Type u} (x : α) : α -> Type u where
-  | refl : TEq x x
+inductive TEq {α : Type u} : α -> α -> Type u where
+  | refl (x : α) : TEq x x
+
+#check TEq.casesOn
 
 -- the abbreviation ≡ is typed with `slash = =`
 infixl:65 " ≡ " => TEq
 
-example : 1 ≡ 1 := TEq.refl
+example : 1 ≡ 1 := TEq.refl 1
 
 -- attribute [refl] allows us to use the rfl tactic on TEq, see the example below.
 -- Discussion where we got the tips for this library: https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/is.20there.20a.20refl.20for.20Type
@@ -25,6 +27,7 @@ example : 1 ≡ 1 := by rfl
 
 -- rfl for Type instead of Prop
 @[match_pattern] def trifle {α : Type u} {a : α} : TEq a a := @TEq.refl α a
+attribute [simp] trifle
 
 example : 1 ≡ 1 := trifle
 
@@ -55,4 +58,16 @@ structure TIff (a b : Type) : Type where
   mpr : b → a
 
 infix:19 " <-> " => TIff
-infix:19 " ↔ "   => TIff
+
+structure Tiso (a b : Type) : Type where
+  intro ::
+  f : a → b
+  f' : b → a
+  ff' : ∀ x, f (f' x) ≡ x
+  f'f : ∀ x, f' (f x) ≡ x
+
+infix:19 " <--> " => Tiso
+
+def Teso {w : α} (P : α -> Type) (Q : α -> Type) := Tiso (P w) (Q w)
+
+infix:19 " <---> " => Teso
