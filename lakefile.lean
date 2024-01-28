77df7c5a9bb218f7c6aea832d22e304844ebb4d4
Walter Schulze <awalterschulze@gmail.com>
2024-01-28 12:07:28 +0000

    Add groundzero dependency and update lean-toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -9,3 +9,7 @@ lean_lib Katydid
 -- dependencies std4, quote4 are obtained transitively through mathlib4
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4"
+
+-- Homotopy Type Theory
+require GroundZero from git
+  "https://github.com/forked-from-1kasper/ground_zero" @ "master"
