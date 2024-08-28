6cae3a0c5f68f29ff3cb13ceca8d11776418f5a7
Walter Schulze <awalterschulze@gmail.com>
2024-08-28 18:55:17 +0100

    move Conal to its own repo https://github.com/katydid/symbolic-automatic-derivatives

--- Katydid.lean
+++ Katydid.lean
@@ -8,13 +8,3 @@ import Katydid.Example.SimpLibrary
 
 import Katydid.Expr.Desc
 import Katydid.Expr.Regex
-
-import Katydid.Conal.Tipe
-import Katydid.Conal.Decidability
-import Katydid.Conal.Function
-import Katydid.Conal.Language
-import Katydid.Conal.Calculus
-import Katydid.Conal.Examples
-import Katydid.Conal.ExamplesProofRelevance
-import Katydid.Conal.Symbolic
-import Katydid.Conal.Automatic
