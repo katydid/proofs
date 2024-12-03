560470f3bbb9ecbf4b36cb00c51ed6bac776017c
Walter Schulze <awalterschulze@gmail.com>
2024-12-03 15:49:03 +0000

    Update SimpLibrary.lean
    fix spelling mistake

--- Katydid/Example/SimpLibrary.lean
+++ Katydid/Example/SimpLibrary.lean
@@ -1,4 +1,4 @@
--- Examlpe of how to use @[simp] to include theorems in simp
+-- Example of how to use @[simp] to include theorems in simp
 -- see https://github.com/leanprover-community/mathlib4/blob/56c1ca9832bdd85620d6b0bbd37ef56818e6b667/Mathlib/Data/Matrix/Basis.lean
 
 namespace SimpLibrary
