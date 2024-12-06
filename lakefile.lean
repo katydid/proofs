144b9b011739e1df3fd692c88093d8ba62115ce7
Walter Schulze <awalterschulze@gmail.com>
2024-12-06 14:48:30 +0000

    update Lean to version 4.14.0

--- lakefile.lean
+++ lakefile.lean
@@ -7,5 +7,4 @@ package katydid
 lean_lib Katydid
 
 -- dependencies std4, quote4 are obtained transitively through mathlib4
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.14.0"
