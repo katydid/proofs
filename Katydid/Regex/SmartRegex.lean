85b33450ff45bd2cd2e22cd344e44543db9498cc
Walter Schulze <awalterschulze@gmail.com>
2024-12-19 21:17:34 +0000

    smart construction proof of concept

--- Katydid/Regex/SmartRegex.lean
+++ Katydid/Regex/SmartRegex.lean
@@ -1,25 +1,10 @@
 import Katydid.Regex.Language
+import Katydid.Regex.SimpleRegex
 
 open List
+open SimpleRegex
 
 namespace SmartRegex
-
-inductive Regex (α: Type): Type where
-  | emptyset : Regex α
-  | emptystr : Regex α
-  | pred : (p: α -> Prop) → [DecidablePred p] → Regex α
-  | or : Regex α → Regex α → Regex α
-  | concat : Regex α → Regex α → Regex α
-  | star : Regex α → Regex α
-
-def denote {α: Type} (r: Regex α): Language.Lang α :=
-  match r with
-  | Regex.emptyset => Language.emptyset
-  | Regex.emptystr => Language.emptystr
-  | Regex.pred p => Language.pred p
-  | Regex.or x y => Language.or (denote x) (denote y)
-  | Regex.concat x y => Language.concat (denote x) (denote y)
-  | Regex.star x => Language.star (denote x)
 
 -- TODO: incorporate more simplification rules into the smart constructor
 def smartOr (x y: Regex α): Regex α :=
@@ -34,10 +19,10 @@ def smartOr (x y: Regex α): Regex α :=
 def smartConcat (x y: Regex α): Regex α :=
   match x with
   | Regex.emptystr => y
-  | _ =>
+  | _otherwise =>
     match y with
     | Regex.emptystr => x
-    | _ => Regex.concat x y
+    | _otherwise => Regex.concat x y
 
 -- TODO: incorporate more simplification rules into the smart constructor
 def smartStar (x: Regex α): Regex α :=
@@ -49,7 +34,36 @@ def smartStar (x: Regex α): Regex α :=
 
 theorem smartOr_is_correct (x y: Regex α):
   denote (Regex.or x y) = denote (smartOr x y) := by
-  sorry
+  cases x with
+  | emptyset =>
+    unfold smartOr
+    simp [denote]
+    exact (Language.simp_or_emptyset_l_is_r (denote y))
+  | emptystr =>
+    cases y <;> (try simp [smartOr])
+    · case emptyset =>
+      simp [denote]
+      exact (Language.simp_or_emptyset_r_is_l _)
+  | pred p =>
+    cases y <;> (try simp [smartOr])
+    · case emptyset =>
+      simp [denote]
+      exact (Language.simp_or_emptyset_r_is_l _)
+  | or x1 x2 =>
+    cases y <;> (try simp [smartOr])
+    · case emptyset =>
+      simp [denote]
+      exact (Language.simp_or_emptyset_r_is_l _)
+  | concat x1 x2 =>
+    cases y <;> (try simp [smartOr])
+    · case emptyset =>
+      simp [denote]
+      exact (Language.simp_or_emptyset_r_is_l _)
+  | star x =>
+    cases y <;> (try simp [smartOr])
+    · case emptyset =>
+      simp [denote]
+      exact (Language.simp_or_emptyset_r_is_l _)
 
 theorem smartConcat_is_correct (x y: Regex α):
   denote (Regex.concat x y) = denote (smartConcat x y) := by
