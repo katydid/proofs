f3a2e57efe30f913bf278cd1997e63ce90f4f8be
Walter Schulze <awalterschulze@gmail.com>
2024-12-23 10:09:07 +0000

    smart or constructor now applies most rules

--- Katydid/Regex/SmartRegex.lean
+++ Katydid/Regex/SmartRegex.lean
@@ -8,32 +8,15 @@ namespace SmartRegex
 
 inductive Predicate (α: Type) where
   | mk
+    (o: Ord α)
     (desc: String)
     (func: α -> Prop)
-    (dec: DecidablePred func)
+    (d: DecidablePred func)
   : Predicate α
 
-def Predicate.desc (p: Predicate α): String :=
+def Predicate.desc {α: Type} (p: Predicate α): String :=
   match p with
-  | ⟨ desc, _, _ ⟩ => desc
-
-def Predicate.func (p: Predicate α): α -> Prop :=
-  match p with
-  | ⟨ _, f, _ ⟩ => f
-
-def Predicate.decfunc (p: Predicate α): DecidablePred p.func := by
-  cases p with
-  | mk desc f dec =>
-    intro a
-    unfold DecidablePred at dec
-    unfold Predicate.func
-    simp
-    apply dec
-
-def evalPredicate (p: Predicate α) (a: α): Bool := by
-  cases p.decfunc a with
-  | isFalse => exact Bool.false
-  | isTrue => exact Bool.true
+  | ⟨ _, desc, _, _ ⟩ => desc
 
 def Predicate.compare (x: Predicate α) (y: Predicate α): Ordering :=
   let xd: String := x.desc
@@ -43,6 +26,34 @@ def Predicate.compare (x: Predicate α) (y: Predicate α): Ordering :=
 instance : Ord (Predicate α) where
   compare: Predicate α → Predicate α → Ordering := Predicate.compare
 
+def Predicate.mkAny [o: Ord α]: Predicate α := by
+  apply @Predicate.mk α o "any"
+  · sorry
+  · exact fun _ => True
+
+def Predicate.mkChar (c: Char): Predicate Char := by
+  apply @Predicate.mk Char inferInstance (String.mk [c])
+  · sorry
+  · exact fun a: Char => a = c
+
+def Predicate.func (p: Predicate α): α -> Prop :=
+  match p with
+  | ⟨ _, _, f, _ ⟩ => f
+
+def Predicate.decfunc (p: Predicate α): DecidablePred p.func := by
+  cases p with
+  | mk _ _ f dec =>
+    intro a
+    unfold DecidablePred at dec
+    unfold Predicate.func
+    simp only
+    apply dec
+
+def evalPredicate (p: Predicate α) (a: α): Bool := by
+  cases p.decfunc a with
+  | isFalse => exact Bool.false
+  | isTrue => exact Bool.true
+
 inductive Regex (α: Type): Type where
   | emptyset : Regex α
   | emptystr : Regex α
@@ -50,6 +61,83 @@ inductive Regex (α: Type): Type where
   | or : Regex α → Regex α → Regex α
   | concat : Regex α → Regex α → Regex α
   | star : Regex α → Regex α
+
+def Regex.compare (x y: Regex α): Ordering :=
+  match x with
+  | Regex.emptyset =>
+    match y with
+    | Regex.emptyset =>
+      Ordering.eq
+    | _ =>
+      Ordering.lt
+  | Regex.emptystr =>
+    match y with
+    | Regex.emptyset =>
+      Ordering.gt
+    | Regex.emptystr =>
+      Ordering.eq
+    | _ =>
+      Ordering.lt
+  | Regex.pred p1 =>
+    match y with
+    | Regex.emptyset =>
+      Ordering.gt
+    | Regex.emptystr =>
+      Ordering.gt
+    | Regex.pred p2 =>
+      Ord.compare p1 p2
+    | _ =>
+      Ordering.lt
+  | Regex.or x1 x2 =>
+    match y with
+    | Regex.emptyset =>
+      Ordering.gt
+    | Regex.emptystr =>
+      Ordering.gt
+    | Regex.pred _ =>
+      Ordering.gt
+    | Regex.or y1 y2 =>
+      match Regex.compare x1 y1 with
+      | Ordering.eq =>
+        Regex.compare x2 y2
+      | o => o
+    | _ =>
+      Ordering.lt
+  | Regex.concat x1 x2 =>
+    match y with
+    | Regex.emptyset =>
+      Ordering.gt
+    | Regex.emptystr =>
+      Ordering.gt
+    | Regex.pred _ =>
+      Ordering.gt
+    | Regex.or _ _ =>
+      Ordering.gt
+    | Regex.concat y1 y2 =>
+      match Regex.compare x1 y1 with
+      | Ordering.eq =>
+        Regex.compare x2 y2
+      | o => o
+    | _ =>
+      Ordering.lt
+  | Regex.star x1 =>
+    match y with
+    | Regex.star y1 =>
+      Regex.compare x1 y1
+    | _ =>
+      Ordering.lt
+
+instance : Ord (Regex α) where
+  compare (x y: Regex α): Ordering := Regex.compare x y
+
+instance : LE (Regex α) where
+  le x y := (Ord.compare x y).isLE
+
+instance : BEq (Regex α) where
+  beq x y := Ord.compare x y == Ordering.eq
+
+def Regex.le (x y: Regex α): Bool :=
+  x <= y
 
 def null (r: Regex α): Bool :=
   match r with
@@ -200,45 +288,32 @@ theorem orToList_is_orFromList (x: Regex α):
   · case or x1 x2 ih1 ih2 =>
     sorry
 
-
--- TODO: incorporate more simplification rules into the smart constructor
--- 1. Get the list of ors (Language.simp_or_assoc)
--- 2. Remove duplicates from the list (create a set) (Language.simp_or_comm and Language.simp_or_idemp and Language.simp_or_assoc)
--- 3. If at any following step the set is size one, simply return
--- 4. If the set contains star (any) then return star (any) (Language.simp_or_universal_r_is_universal and Language.simp_or_universal_l_is_universal)
--- 5. Delete emptyset from the set (Language.simp_or_emptyset_r_is_l and Language.simp_or_emptyset_l_is_r)
--- 6. If any of the set is nullable, remove emptystr from the set (Language.simp_or_emptystr_null_r_is_r and Language.simp_or_null_l_emptystr_is_l)
--- 7. create a sorted list from the set (Language.simp_or_assoc and Language.simp_or_comm)
+-- 1. If x or y is emptyset then return the other (Language.simp_or_emptyset_r_is_l and Language.simp_or_emptyset_l_is_r)
+-- 2. If x or y is star (any) then return star (any) (Language.simp_or_universal_r_is_universal and Language.simp_or_universal_l_is_universal)
+-- 3. Get the lists of ors using orToList (Language.simp_or_assoc)
+-- 4. Merge sort the sorrted lists (Language.simp_or_comm and Language.simp_or_assoc)
+-- 5. Remove duplicates from the list (or create a set) (Language.simp_or_idemp)
+-- 6. If at any following step the set is size one, simply return
+-- TODO: 7. If any of the set is nullable, remove emptystr from the set (Language.simp_or_emptystr_null_r_is_r and Language.simp_or_null_l_emptystr_is_l)
+-- 8. Reconstruct Regex.or from the list using orFromList (Language.simp_or_assoc)
 def smartOr (x y: Regex α): Regex α :=
   match x with
   | Regex.emptyset => y
+  | Regex.star (Regex.pred (Predicate.mk _ "any" _ _)) => x
   | _ =>
-    match y with
-    | Regex.emptyset => x
-    | _ => Regex.or x y
-
-private theorem smartOr_is_or_righthand (x y: Regex α) (hx_emptyset: x ≠ Regex.emptyset):
-  denote (Regex.or x y) = denote (smartOr x y) := by
-  cases y <;> (try simp [smartOr])
-  · case emptyset =>
-    simp [denote]
-    exact (Language.simp_or_emptyset_r_is_l (denote x))
+  match y with
+  | Regex.emptyset => x
+  | Regex.star (Regex.pred (Predicate.mk _ "any" _ _)) => y
+  | _ =>
+    -- it is implied that xs is sorted, given it was created using smartOr
+    let xs := orToList x
+    let ys := orToList y
+    -- merge the sorted lists, resulting in a sorted list
+    let ors := NonEmptyList.merge xs ys
+    -- remove duplicates from sorted list using erase repititions
+    let uniqueOrs := NonEmptyList.eraseReps ors
+    orFromList uniqueOrs
 
 theorem smartOr_is_or (x y: Regex α):
   denote (Regex.or x y) = denote (smartOr x y) := by
-  have hy := smartOr_is_or_righthand x y
-  match x with
-  | Regex.emptyset =>
-    unfold smartOr
-    simp [denote]
-    exact (Language.simp_or_emptyset_l_is_r (denote y))
-  | Regex.emptystr =>
-    exact hy (by simp)
-  | Regex.pred p =>
-    exact hy (by simp)
-  | Regex.or x1 x2 =>
-    exact hy (by simp)
-  | Regex.concat x1 x2 =>
-    exact hy (by simp)
-  | Regex.star x1 =>
-    exact hy (by simp)
+  sorry
--- Katydid/Std/NonEmptyList.lean
+++ Katydid/Std/NonEmptyList.lean
@@ -1,8 +1,56 @@
+import Katydid.Std.Ordering
+import Katydid.Std.Lists
+
 structure NonEmptyList (α: Type) where
   head : α
   tail : List α
 
 namespace NonEmptyList
 
+def compare [o: Ord α] (xs ys: NonEmptyList α): Ordering :=
+  match xs with
+  | NonEmptyList.mk x' xs' =>
+  match ys with
+  | NonEmptyList.mk y' ys' =>
+    Ordering.lex (Ord.compare x' y') (Ord.compare xs' ys')
+
+instance [Ord α] : Ord (NonEmptyList α) where
+  compare := compare
+
 def cons (x: α) (xs: NonEmptyList α): NonEmptyList α :=
   NonEmptyList.mk x (xs.head :: xs.tail)
+
+def toList (xs: NonEmptyList α): List α :=
+  match xs with
+  | NonEmptyList.mk head tail =>
+    head :: tail
+
+instance [Ord α] : LE (NonEmptyList α) where
+  le (x: NonEmptyList α) (y: NonEmptyList α) :=
+    LE.le x.toList y.toList
+
+def merge' [Ord α] (xs: NonEmptyList α) (ys: NonEmptyList α): List α :=
+  List.merge (xs.toList) (ys.toList) (fun x y => (Ord.compare x y).isLE)
+
+def merge [Ord α] (xs: NonEmptyList α) (ys: NonEmptyList α): NonEmptyList α :=
+  match xs with
+  | NonEmptyList.mk x' xs' =>
+  match ys with
+  | NonEmptyList.mk y' ys' =>
+  match Ord.compare x' y' with
+  | Ordering.eq =>
+    NonEmptyList.mk x' (Lists.merge xs' ys')
+  | Ordering.lt =>
+    NonEmptyList.mk x' (Lists.merge xs' (y'::ys'))
+  | Ordering.gt =>
+    NonEmptyList.mk y' (Lists.merge (x'::xs') ys')
+
+def eraseReps [BEq α] (xs: NonEmptyList α): NonEmptyList α :=
+  match xs with
+  | NonEmptyList.mk x' xs' =>
+    match xs' with
+    | [] => xs
+    | (x''::xs'') =>
+      if x' == x''
+      then eraseReps (NonEmptyList.mk x'' xs'')
+      else NonEmptyList.mk x' (List.eraseReps xs')
