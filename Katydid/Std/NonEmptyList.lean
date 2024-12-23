c936831ed52a4277eb0f25f3c2961529060e11ac
Walter Schulze <awalterschulze@gmail.com>
2024-12-23 14:11:17 +0000

    NonEmptyList.mergeReps

--- Katydid/Std/NonEmptyList.lean
+++ Katydid/Std/NonEmptyList.lean
@@ -53,34 +53,17 @@ def eraseReps [BEq α] (xs: NonEmptyList α): NonEmptyList α :=
     | (x''::xs'') =>
       if x' == x''
       then NonEmptyList.eraseReps (NonEmptyList.mk x'' xs'')
-      else NonEmptyList.mk x' (List.eraseReps xs')
-
-def mergeRep [BEq α] [Ord α] (x: α) (ys zs : List α): NonEmptyList α :=
-  match ys, zs with
-  | [], [] =>
-    NonEmptyList.mk x []
-  | (y::ys), [] =>
-    match Ord.compare x y with
-    | Ordering.eq =>
-        NonEmptyList.eraseReps (NonEmptyList.mk y ys)
-    | Ordering.lt =>
-        NonEmptyList.cons x (NonEmptyList.eraseReps (NonEmptyList.mk y ys))
-    | Ordering.gt =>
-        NonEmptyList.cons y (NonEmptyList.eraseReps (NonEmptyList.mk x ys))
-  | [], (y::ys) =>
-    match Ord.compare x y with
-    | Ordering.eq => eraseReps (NonEmptyList.mk y ys)
-    | Ordering.lt => NonEmptyList.cons x (eraseReps (NonEmptyList.mk y ys))
-    | Ordering.gt => NonEmptyList.cons y (eraseReps (NonEmptyList.mk x ys))
-  | (y::ys'), (z::zs') =>
-    sorry
+      else NonEmptyList.mk x' (Lists.eraseReps xs')
 
 def mergeReps [BEq α] [Ord α] (xs ys : NonEmptyList α): NonEmptyList α :=
   match xs, ys with
-  | NonEmptyList.mk x [], NonEmptyList.mk y ys' =>
+  | NonEmptyList.mk x xs, NonEmptyList.mk y ys =>
     match Ord.compare x y with
-    | Ordering.eq => NonEmptyList.eraseReps ys
-    | Ordering.lt => NonEmptyList.cons x (NonEmptyList.eraseReps ys)
-    | Ordering.gt => NonEmptyList.cons y (NonEmptyList.eraseReps (NonEmptyList.mk x ys'))
-  | NonEmptyList.mk x xs', NonEmptyList.mk y ys' =>
-   sorry
+    | Ordering.eq =>
+        match xs with
+        | [] =>
+            NonEmptyList.mk y ys
+        | (x'::xs') =>
+            NonEmptyList.mergeReps (NonEmptyList.mk x' xs') (NonEmptyList.mk y ys)
+    | Ordering.lt => NonEmptyList.mk x (Lists.mergeReps xs (y::ys))
+    | Ordering.gt => NonEmptyList.mk y (Lists.mergeReps (x::xs) ys)
